bb83b78ed8526e57bce6a6cb7612d75356be3b30
Yang-Kichang <ykcha9@gmail.com>
2018-04-16 15:32:39 +0900

    Delete DataField.py

--- DataField.py
+++
@@ -1,5 +0,0 @@
-#ID="minjea917"
-ID = 'slls2008'
-#PASSWD="rnjsalswo10"
-PASSWD = 'ehdqkddk123'
-TESTCOOKIE="ASPSESSIONIDQAABSSBD=LCAGEGKBEBDMGHGLEPJBOGIJ; avatar=log%5Favatar; hp=4f8863b88cc080e3b4a84c298efd1e3d3727; phone=4f8863b88cc080e3b4a84c298efd1e3d3727; memno=CY573889; sms=0; zip=+++++++; apprice=12; tel=%2D%2D; rflag=1; email=minjea917%40naver%2Ecom; jumin=HHyyXm%2DYc97d1377407cdc7482d23f2a350b0d36; name=%EA%B6%8C%EB%AF%BC%EC%9E%AC; help=1%2C3%2C4%2C7; addr1=; cid=minjea917; ecl%5Fdec%5Fhp=010%2D6679%2D3727; golf=0; idno=HHyyXm%2DYc97d1377407cdc7482d23f2a350b0d36; id=minjea917; Vender=; addr2=; memck=1; power=m; memtype=31"
